122bb462f652f385cc3eaba5636f00a06e366b6f
Cesar Lopez <cesarl@outlook.com>
2015-09-22 10:26:14 -0600

    S3 : Object Oriented and inheritance

---
+++ S3/person.py
@@ -0,0 +1,24 @@
+#Define Class person:
+class Person:
+    def __init__(self, first, middle, last, age):
+        self.first = first
+        self.middle = middle
+        self.last = last
+        self.age = age
+        
+    def __str__(self):
+        return self.first + ' ' + self.middle + ' ' + self.last + ' '+\
+            str(self.age)
+        
+    def initials(self):
+        return self.first[0] + self.middle[0] + self.last[0]
+    
+    def changeAge(self, amount):
+        self.age += amount
+
+#Create an object of type Person        
+aPerson = Person('Jane', 'Q', 'Public', 27)
+print(aPerson)
+aPerson.changeAge(2)
+print(aPerson)
+print(aPerson.initials())
---
+++ S3/shapes.py
@@ -0,0 +1,30 @@
+#Define Class Shape
+class Shape:
+    def __init__(self, xcor, ycor):
+        self.x = xcor
+        self.y = ycor
+    
+    def __str__(self):
+        return 'x: ' + str(self.x) + ' y: ' + str(self.y)
+    
+    def move(self, x1, y1):
+        self.x += x1
+        self.y += y1
+        
+#Define Rectangle class thet inherits from Shape Class
+class Rectangle(Shape):
+    def __init__(self, xcor, ycor, width, height):
+        Shape.__init__(self, xcor, ycor)
+        self.width = width
+        self.height = height
+    
+    def __str__(self):
+        return Shape.__str__(self) + ' width: ' + str(self.width) + \
+            ' height: ' + str(self.height)
+        
+#Create a Rectangle object
+rec = Rectangle(5, 10, 8, 9)
+print(rec)
+rec.move(10, 12)
+print(rec)
+        
