bdafa124556aae0683fff6b48fd7031f5e43c01b
Cesar Lopez <cesarl@outlook.com>
2015-09-28 16:55:24 -0600

    S7 & S8 : CGI Programming, Form Processing & Db Web Programming

---
+++ S7/cgi-bin/hellocgi.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.stdout.write("Content-type: text/html \r\n\r\n")
+sys.stdout.write("<!doctype html><html><hrad><title>Hello CGI</title></head>")
+sys.stdout.write("<body><h2>Hello CGI</h2></body></html>")
---
+++ S7/cgi-bin/querys.py
@@ -0,0 +1,18 @@
+import cgi, os, sys
+
+sys.stdout.write("Content-type: text/html \r\n\r\n")
+sys.stdout.write("")
+sys.stdout.write("<html><body>")
+sys.stdout.write("<h2>Query String</h2>")
+
+form = cgi.FieldStorage()
+for field in form.keys():
+    sys.stdout.write("%s -> %s<br />" % (field, form[field].value))
+    
+sys.stdout.write("</body></html>")
+
+#Run cgi server like this:
+#  python -m CGIHTTPServer
+#Call like this:
+#  http://localhost:8000/cgi-bin/querys.py?Paul=bass&Ringo=drums
+        
